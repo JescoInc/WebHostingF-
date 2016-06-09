dcabbc4f0e0b84266f761bc4b47f05837b9f4d45
Jesse Glover <silverdrakeproductions@gmail.com>
2016-06-09 03:24:49 -0400

    Added full source code of the project

---
+++ NancyHosting/SelfHost/SelfHostExample.fs
@@ -0,0 +1,8 @@
+﻿open System
+open Nancy.Hosting.Self
+
+let host = new NancyHost(new Uri("http://localhost:1234"))
+
+printf "Running on http://localhost:1234"
+host.Start()
+Console.ReadLine() |> ignore
---
+++ NancyHosting/WCFHost/WcfHostExample.fs
@@ -0,0 +1,10 @@
+﻿open System
+open System.ServiceModel
+open System.ServiceModel.Web
+open Nancy.Hosting.Wcf
+
+let host = new WebServiceHost(new NancyWcfGenericService(), new Uri("http://localhost:1234/base/"))
+host.AddServiceEndpoint(typeof<NancyWcfGenericService>, new WebHttpBinding(), "")
+host.Open()
+printf "Now serving host : http://localhost:1234/base/ "
+Console.ReadLine() |> ignore
